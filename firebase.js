0a43f32782431ff532ecc88a8950f09a421a9c56
pranaytejareddy1996 <ch.pranaytejareddy@gmail.com>
2021-06-20 17:41:27 +0530

    day 3

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBa9hSStjI65SWGX_VtzbLCXMg6XnEQLLA",
+    authDomain: "amaze-2-yt.firebaseapp.com",
+    projectId: "amaze-2-yt",
+    storageBucket: "amaze-2-yt.appspot.com",
+    messagingSenderId: "360065791395",
+    appId: "1:360065791395:web:490ca127dbd299cdfb9616"
+  };
